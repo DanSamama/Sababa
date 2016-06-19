a8eaebb5c7610dcd2e4f6ec8b22983faa8e3868d
Stephen Skoler <skolerst@gmail.com>
2016-06-19 16:26:07 +0300

    Update sababa.sql
    changed the example emails to all be unique

--- sababa.sql
+++ sababa.sql
@@ -38,28 +38,28 @@ USE sababa_tbl;
 /* NULL is here because the id is auto implemented */
 
 /* the guys that need help */
-INSERT INTO sababa_tbl (NULL, 0, 'test@gmail.com', 'pwd1', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 0, 'test@gmail.com', 'pwd2', 'Ely', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
-		       (NULL, 0, 'test@gmail.com', 'pwd3', 'Nathalie', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 0, 'test@gmail.com', 'pwd4', 'Daniel', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
-		       (NULL, 0, 'test@gmail.com', 'pwd5', 'Shlomo', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 0, 'test@gmail.com', 'pwd6', 'Moshe', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 0, 'test@gmail.com', 'pwd7', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
-		       (NULL, 0, 'test@gmail.com', 'pwd8', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 0, 'test@gmail.com', 'pwd9', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
-		       (NULL, 0, 'test@gmail.com', 'pwd0', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5);
+INSERT INTO sababa_tbl (NULL, 0, 'test0@gmail.com', 'pwd1', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 0, 'test1@gmail.com', 'pwd2', 'Ely', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
+		       (NULL, 0, 'test2@gmail.com', 'pwd3', 'Nathalie', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 0, 'test3@gmail.com', 'pwd4', 'Daniel', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
+		       (NULL, 0, 'test4@gmail.com', 'pwd5', 'Shlomo', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 0, 'test5@gmail.com', 'pwd6', 'Moshe', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 0, 'test6@gmail.com', 'pwd7', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
+		       (NULL, 0, 'test7@gmail.com', 'pwd8', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 0, 'test8@gmail.com', 'pwd9', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
+		       (NULL, 0, 'test9@gmail.com', 'pwd0', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5);
 
 /* the helpers */
-INSERT INTO sababa_tbl (NULL, 1, 'test@gmail.com', 'pwd10', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 1, 'test@gmail.com', 'pwd11', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
-		       (NULL, 1, 'test@gmail.com', 'pwd12', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 1, 'test@gmail.com', 'pwd13', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
-		       (NULL, 1, 'test@gmail.com', 'pwd14', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2),
-		       (NULL, 1, 'test@gmail.com', 'pwd15', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
-		       (NULL, 1, 'test@gmail.com', 'pwd16', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
-		       (NULL, 1, 'test@gmail.com', 'pwd17', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 1, 'test@gmail.com', 'pwd19', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
-		       (NULL, 1, 'test@gmail.com', 'pwd20', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5);
+INSERT INTO sababa_tbl (NULL, 1, 'test10@gmail.com', 'pwd10', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 1, 'test11@gmail.com', 'pwd11', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
+		       (NULL, 1, 'test12@gmail.com', 'pwd12', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 1, 'test13@gmail.com', 'pwd13', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
+		       (NULL, 1, 'test14@gmail.com', 'pwd14', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2),
+		       (NULL, 1, 'test15@gmail.com', 'pwd15', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3),
+		       (NULL, 1, 'test16@gmail.com', 'pwd16', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 4),
+		       (NULL, 1, 'test17@gmail.com', 'pwd17', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 1, 'test18@gmail.com', 'pwd19', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5),
+		       (NULL, 1, 'test19@gmail.com', 'pwd20', 'Jeremy', 'ATIA', '+972 54239891', 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 5);
 
 
 
